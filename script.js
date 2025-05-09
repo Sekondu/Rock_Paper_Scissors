2b298800124aa1436da52734b1eb7387e4d3d6e6
musashi <rwid2005@gmail.com>
2025-05-09 17:46:17 +0300

    low UX version of Rock,Paper,Scissors

--- script.js
+++ script.js
@@ -14,36 +14,76 @@ else if(num===3)
     return "Scissors";
 }
 }
-function getHumanChoice()
-{
-    let choice=prompt("enter your choice");
-    return choice[0].toUpperCase()+choice.toLowerCase().slice(1);
-}
-let humanScore=0;
+let num_of_plays=6;
+let ChoiceWinner=document.querySelector(".ChoiceWinner");
 let computerScore=0;
-function playRound()
+let humanScore=0;
+let score=document.querySelector(".Score");
+function ScoreUpdate()
 {
-    let C_choice=getComputerChoice();
-    let H_choice=getHumanChoice();
-    console.log(`computer choice is ${C_choice} and human choice is ${H_choice}`)
+    if(num_of_plays<5)
+    {
+        let C_choice=getComputerChoice();
 
-    if (C_choice==="Rock" && H_choice==="Scissors" || C_choice==="Scissors" && H_choice==="Paper" || C_choice==="Paper" && H_choice==="Rock") 
-    {
-        computerScore++;
+        ChoiceWinner.textContent=`computer choice is ${C_choice} and human choice is ${H_choice}`;
+    
+        if (C_choice==="Rock" && H_choice==="Scissors" || C_choice==="Scissors" && H_choice==="Paper" || C_choice==="Paper" && H_choice==="Rock") 
+        {
+            computerScore++;
+        }
+        else if(H_choice==="Rock" && C_choice==="Scissors" || H_choice==="Scissors" && C_choice==="Paper" || H_choice==="Paper" && C_choice==="Rock")
+        {
+            humanScore++;
+        }
+        score.textContent=`Player Score: ${humanScore} || Computer Score: ${computerScore}`;
+        num_of_plays++;
     }
-    else if(H_choice==="Rock" && C_choice==="Scissors" || H_choice==="Scissors" && C_choice==="Paper" || H_choice==="Paper" && C_choice==="Rock")
-    {
-        humanScore++;
+    else if(num_of_plays===5){
+        if(humanScore>computerScore)
+        {
+            ChoiceWinner.textContent="Player Win!";
+        }
+        else if(computerScore>humanScore)
+        {
+            ChoiceWinner.textContent="Computer Win!";
+        }
+        else{
+            ChoiceWinner.textContent="Draw!";
+        }
+
     }
+    
 }
-function playGame()
+
+let Round=document.querySelector(".play");
+
+Round.addEventListener("click",() =>
 {
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    console.log(`computer score is ${computerScore} and human score is ${humanScore}`);
+    num_of_plays=0;
+    ChoiceWinner.textContent="Choose an Option";
     humanScore=0;
     computerScore=0;
+    score.textContent=`Player Score: ${humanScore} || Computer Score: ${computerScore}`;
+})
+
+let buttons=document.querySelectorAll("div>button");
+buttons.forEach(button =>
+{
+    button.addEventListener("click", (event) =>
+    {
+        switch(event.target.className)
+        {
+            case "Rock":
+            H_choice="Rock";
+            break;
+            case "Paper":
+            H_choice="Paper";
+            break;
+            case "Scissors":
+            H_choice="Scissors";
+            break;
+        }
+        ScoreUpdate();
+    })
 }
+)
